ed4a10f2bfaf9230e9f897a1478e5db92fb864e7
Beatrix Bicomong <b.bicomong@share.epsb.ca>
2022-09-13 11:38:07 -0600

    Added recursive binary search

--- b_binSearch.py
+++ b_binSearch.py
@@ -16,7 +16,7 @@ def createArray(SIZE):
     :return: LIST (int)
     """
     NUMBERS = []
-    for i in range(2*SIZE):
+    for i in range(2 * SIZE):
         if randrange(2) == 1:
             NUMBERS.append(i)
     return NUMBERS
@@ -34,7 +34,7 @@ def binarySearch(LIST, VALUE):
     END = len(LIST) - 1
 
     while START <= END:
-        MIDPOINT = (START + END) // 2 # Floor divide for an int
+        MIDPOINT = (START + END) // 2  # Floor divide for an int
         if LIST[MIDPOINT] == VALUE:
             return True
         elif VALUE > LIST[MIDPOINT]:
@@ -44,16 +44,36 @@ def binarySearch(LIST, VALUE):
     return False
 
 
+def recurBinSearch(LIST, VALUE):
+    """
+    recursive binary search
+    :param LIST: list (int)
+    :param VALUE: int
+    :return: bool
+    """
+    if len(LIST) > 0:
+        MIDPOINT = len(LIST) // 2
+        if LIST[MIDPOINT] == VALUE:
+            return True
+        elif VALUE < LIST[MIDPOINT]:
+            return recurBinSearch(LIST[:MIDPOINT], VALUE)
+        else:
+            return recurBinSearch(LIST[MIDPOINT + 1:], VALUE)
+    else:
+        return False
+
+
 if __name__ == "__main__":
-    NUMBERS = createArray(10000000)
+    NUMBERS = createArray(2000000)
 
     TIMES = []
-    for i in range(30):
+    for i in range(1):
         NUMBER = NUMBERS[randrange(len(NUMBERS))]
         print(NUMBER)
 
         START = perf_counter()
-        FOUND = binarySearch(NUMBERS, NUMBER)
+        # FOUND = binarySearch(NUMBERS, NUMBER)
+        FOUND = recurBinSearch(NUMBERS, NUMBER)
         END = perf_counter()
 
         print(FOUND)
@@ -61,4 +81,3 @@ if __name__ == "__main__":
         TIMES.append(END - START)
 
     print(mean(TIMES))
-
